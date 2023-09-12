f22aa8384e3081c4cb67b88c80e05bdcfe7adedc
Gaurav Jeena <gauravjeena@outlook.com>
2023-09-12 19:46:17 +0000

    TA code review feedback incorporated

--- lesson_3/tictactoe1.rb
+++ lesson_3/tictactoe1.rb
@@ -1,9 +1,9 @@
 INITIAL = " "
 USER_MARKER = "X"
 COMP_MARKER = "O"
-WIN_ARR = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
-           [1, 4, 7], [2, 5, 8], [3, 6, 9],
-           [1, 5, 9], [3, 5, 7]]
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                 [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                 [1, 5, 9], [3, 5, 7]]
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -15,8 +15,27 @@ def intitialze_board
   new_board
 end
 
+def initialize_demo_board
+  demo_board = {}
+  (1..9).each { |num| demo_board[num] = num }
+  demo_board
+end
+
+def game_intro
+  demo_board = initialize_demo_board
+  display_board(demo_board)
+  prompt("Welcome to the game of Tic Tac Toe!")
+  puts ""
+  prompt("You're #{USER_MARKER}. Computer is #{COMP_MARKER}.")
+  prompt("We are playing best of 5 so first to 3 wins, tie's don't count")
+  prompt("Square numbers are shown in the above board")
+  prompt("You can mark a square by entering the corresponding square number")
+  puts ""
+end
+
 # rubocop:disable Metrics/AbcSize
 def display_board(brd_hsh)
+  system 'clear'
   puts ""
   puts "   |   |"
   puts " #{brd_hsh[1]} | #{brd_hsh[2]} | #{brd_hsh[3]}"
@@ -41,20 +60,23 @@ def joinor(valid_moves, separator = ", ", connector = "or")
   end
 end
 
-def invalid_input?(input)
-  if input.to_i == 0 || input.to_i > 9
-    prompt("Invalid input")
-    return true
-  end
-  false
+def invalid_input?(user_move)
+  user_move.to_i == 0 || user_move.to_i > 9
+end
+
+def prompt_invalid_input
+  puts "******************"
+  prompt("Invalid input! Input again.")
+  puts "******************"
+end
+
+def prompt_choice_taken?
+  prompt("This choice is taken")
+  prompt("Go again...")
 end
 
 def choice_taken?(brd, user_move)
-  if brd[user_move] == USER_MARKER || brd[user_move] == COMP_MARKER
-    prompt("This choice is taken")
-    prompt("Go again...")
-    true
-  end
+  brd[user_move] == USER_MARKER || brd[user_move] == COMP_MARKER
 end
 
 def go_first
@@ -68,8 +90,8 @@ def go_first
   end
 end
 
-def display_result_per_round(winner)
-  case winner
+def display_result_per_round(round_winner)
+  case round_winner
   when "Player"
     prompt("You won!!!!")
   when "Computer"
@@ -80,16 +102,25 @@ def display_result_per_round(winner)
 end
 
 def someone_won?(brd)
-  WIN_ARR.each do |win_arr|
-    return "Player" if win_arr.all? { |value| brd[value] == USER_MARKER }
-    return "Computer" if win_arr.all? { |value| brd[value] == COMP_MARKER }
+  WINNING_LINES.each do |win_arr|
+    if win_arr.all? { |value| brd[value] == USER_MARKER } ||
+       win_arr.all? { |value| brd[value] == COMP_MARKER }
+      return true
+    end
   end
   false
 end
 
-def best_of_five_winner(user_score, comp_score)
+def determine_who_won(brd)
+  WINNING_LINES.each do |win_arr|
+    return "Player" if win_arr.all? { |value| brd[value] == USER_MARKER }
+    return "Computer" if win_arr.all? { |value| brd[value] == COMP_MARKER }
+  end
+end
+
+def display_best_of_five_winner(user_score, comp_score)
   if user_score > comp_score
-    prompt("You are the winner of best of 5, Congratulations!")
+    prompt("You are the winner of best of 5, Congratulations!!")
   elsif comp_score > user_score
     prompt("Sorry computer won this best of 5 :(")
   end
@@ -102,7 +133,7 @@ end
 # In the case where there is no threat and no defense then go for offense
 # rather than a random move
 def avoid_useless_move(brd, valid_moves)
-  WIN_ARR.each do |win_arr|
+  WINNING_LINES.each do |win_arr|
     if brd.values_at(*win_arr).count(" ") == 1
       avoid_moves_arr = brd.values_at(*win_arr).select { |v| v == " " }
       useful_move_arr = valid_moves - avoid_moves_arr
@@ -114,7 +145,7 @@ def avoid_useless_move(brd, valid_moves)
 end
 
 def find_immediate_threat?(brd)
-  WIN_ARR.each do |win_arr|
+  WINNING_LINES.each do |win_arr|
     if brd.values_at(*win_arr).count(USER_MARKER) == 2 &&
        brd.values_at(*win_arr).any?(" ")
       user_marked_arr = brd.keys.select do |k|
@@ -128,7 +159,7 @@ def find_immediate_threat?(brd)
 end
 
 def find_offensive_move?(brd)
-  WIN_ARR.each do |win_arr|
+  WINNING_LINES.each do |win_arr|
     if brd.values_at(*win_arr).count(COMP_MARKER) == 2 &&
        brd.values_at(*win_arr).any?(" ")
       currently_used_spaces = brd.keys.select do |k|
@@ -143,40 +174,45 @@ def find_offensive_move?(brd)
 end
 
 # rubocop:disable Metrics/MethodLength
-def computer_input!(brd, valid_moves)
+def get_computer_move!(brd, valid_moves)
+  prompt("Computer's turn now...")
+  sleep(1.5)
+
   threat = find_immediate_threat?(brd)
   offense = find_offensive_move?(brd)
   useful_move = avoid_useless_move(brd, valid_moves)
 
   if offense != false               # go for offense if available
-    comp_input = offense
+    computer_move = offense
   elsif threat != false             # if there is a threat then defend
-    comp_input = threat
+    computer_move = threat
   elsif valid_moves.include?(5)     # if middle cell is available then take it
-    comp_input = 5
+    computer_move = 5
   elsif useful_move != false        # no threat yet, then go for offense
-    comp_input = useful_move
-  elsif threat == false             # pick random
-    comp_input = valid_moves.sample
+    computer_move = useful_move
+  else                              # pick random
+    computer_move = valid_moves.sample
   end
 
-  brd[comp_input] = COMP_MARKER
-  empty_spaces(comp_input, valid_moves)
-end
-
-def empty_spaces(move, valid_moves)
-  valid_moves.delete_if { |index| index == move }
+  brd[computer_move] = COMP_MARKER
+  display_board(brd)
+  computer_move
 end
 # rubocop:enable Metrics/MethodLength
 
-def display_score(user_score, comp_score)
-  prompt("You have won #{user_score} games")
-  prompt("Computer has won #{comp_score} games")
-  puts "****************************************"
+def update_valid_moves!(move, valid_moves)
+  valid_moves.delete_if { |index| index == move }
 end
 
-def keep_score(winner, user_score, comp_score)
-  case winner
+def display_score(user_score, comp_score)
+  prompt("You have won #{user_score} out of 3 games in a best of 5")
+  prompt("Computer has won #{comp_score} out of 3 games in a best of 5")
+  puts "****************************************"
+  puts ""
+end
+
+def keep_score(round_winner, user_score, comp_score)
+  case round_winner
   when "Player"
     user_score += 1
   when "Computer"
@@ -185,51 +221,51 @@ def keep_score(winner, user_score, comp_score)
   [user_score, comp_score]
 end
 
-def user_input!(brd, valid_moves)
-  user_move = ''
+def get_user_move!(brd, valid_moves, user_move = '')
   loop do
-    display_board(brd)
-
     prompt("Your turn, enter a square from these #{joinor(valid_moves)}")
     user_move = gets.to_i
 
-    next if invalid_input?(user_move)
+    if invalid_input?(user_move)
+      prompt_invalid_input
+      next
+    end
 
     if choice_taken?(brd, user_move)
+      prompt_choice_taken?
       next
-    else
-      brd[user_move] = USER_MARKER
-      break
     end
+
+    brd[user_move] = USER_MARKER
+    break
   end
-  empty_spaces(user_move, valid_moves)
+  user_move
 end
 
-def lets_play(brd, who_goes)
+def play_a_round(brd, who_goes)
   valid_moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  display_board(brd)
 
-  computer_input!(brd, valid_moves) if who_goes == "Computer"
+  if who_goes == "Computer"
+    computer_move = get_computer_move!(brd, valid_moves)
+    update_valid_moves!(computer_move, valid_moves)
+  end
 
   loop do
-    user_input!(brd, valid_moves)
+    user_move = get_user_move!(brd, valid_moves)
+    update_valid_moves!(user_move, valid_moves)
     display_board(brd)
-    break if !!someone_won?(brd)
+    break if someone_won?(brd) || valid_moves.empty?
 
-    if !valid_moves.empty?
-      prompt("Computer's turn now...")
-      sleep(1)
-      computer_input!(brd, valid_moves)
-    end
-    break if !!someone_won?(brd) || valid_moves.empty?
+    computer_move = get_computer_move!(brd, valid_moves)
+    update_valid_moves!(computer_move, valid_moves)
+
+    break if someone_won?(brd) || valid_moves.empty?
   end
 end
 
 def determine_who_goes(who_goes)
-  if who_goes == "Computer"
-    "Player"
-  else
-    "Computer"
-  end
+  who_goes == "Computer" ? "Player" : "Computer"
 end
 
 def display_who_goes(who_goes)
@@ -243,34 +279,37 @@ end
 # rubocop:disable Metrics/MethodLength
 def main_game(who_goes, user_score = 0, comp_score = 0)
   times_played = 1
-  winner = ""
+  round_winner = ""
   loop do
     display_who_goes(who_goes)
     display_game_number(times_played)
+    puts("Hit Enter ↵ to start")
+    gets.chomp
     board = intitialze_board
-    lets_play(board, who_goes)
-    winner = someone_won?(board)
+    play_a_round(board, who_goes)
+    round_winner = determine_who_won(board)
     display_board(board)
-    display_result_per_round(winner)
-    user_score, comp_score = keep_score(winner, user_score, comp_score)
+    display_result_per_round(round_winner)
+    user_score, comp_score = keep_score(round_winner, user_score, comp_score)
     display_score(user_score, comp_score)
     times_played += 1
     who_goes = determine_who_goes(who_goes)
     break if user_score >= 3 || comp_score >= 3
   end
-  best_of_five_winner(user_score, comp_score)
+  display_best_of_five_winner(user_score, comp_score)
 end
 # rubocop:enable Metrics/MethodLength
-
-prompt("Welcome to the game of Tic Tac Toe!")
-prompt("We are playing best of 5 so first to 3 wins, tie's don't count")
+system 'clear'
 
 loop do
+  game_intro
   who_goes = go_first
   main_game(who_goes)
+  puts "****************************************"
   prompt("Do you want to play again? y/n")
   choice = gets.chomp
   break if choice.start_with?("n")
+  system 'clear'
 end
 
 prompt("Thank you for playing Tic Tac Toe!")
